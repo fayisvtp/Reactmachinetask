3867e2665296705a469645155d4a9160998afa81
fayisvtp <fayizvtp@gmail.com>
2024-02-03 15:00:25 +0530

    desgn

--- UI/dashboard/src/App.jsx
+++ UI/dashboard/src/App.jsx
@@ -1,33 +1,30 @@
-import AdminProfile from "./component/AdminProfile"
-import Graphdata from "./component/Graphdata"
-import PieChartComponent from "./component/PieChartComponent "
-import TableData from "./component/TableData"
-
+import AdminProfile from "./component/AdminProfile";
+import Graphdata from "./component/Graphdata";
+import PieChartComponent from "./component/PieChartComponent ";
+import TableData from "./component/TableData";
 
 function App() {
-
-
   return (
-    <div className="bg-success">
-      <div className="col-12 row d-flex">
-        <div className="col-7 bg-danger">
-        <PieChartComponent/>
+    <div className="m-auto" style={{ backgroundColor: '#BBE2EC' }}>
+      <h4>Good Morning 🌞</h4>
+      <div className="col-12 row  d-flex justify-content-between">
+        <div className="col-7 bg-light shadow rounded">
+          <PieChartComponent />
         </div>
-        <div className="col-5 bg-wanrning">
-        <Graphdata/>
+        <div className="col-4 bg-light shadow rounded">
+          <Graphdata />
         </div>
-        <div className="col-9 ">
-        <TableData/>
-        <div className="col-3  bg-dark">
-          fsfskjjhgkjsh
-        </div>
+        <div className="col-12  d-flex gap-3 m-2">
+          <div className="col-9 bg-light shadow rounded">
+            <TableData />
+          </div>
+          <div className="col-3 bg-dark">
+            <AdminProfile />
+          </div>
         </div>
       </div>
-
-     
-   
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- UI/dashboard/src/component/AdminProfile.jsx
+++ UI/dashboard/src/component/AdminProfile.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 export default function AdminProfile() {
   return (
-    <div className="col3 profile">
-        fsdgfgh
+    <div className="profile">
+       <div className="card card-shadow bg-success">
+        <div className="image ">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/C1ccD-9rzTS._CR0,0,3840,2880_._SL1000_.jpg" alt=""style={{width:'100%'}} />
+        </div>
+      <div className='align-items-center'>
+      <h2 className="userName align-items-center">name</h2>
+
+      </div>
+
+       </div>
        <div className="card  bg-dark"></div>
     </div>
   )
